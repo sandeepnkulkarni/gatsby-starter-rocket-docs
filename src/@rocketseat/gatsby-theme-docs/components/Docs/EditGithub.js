5734f082e03aedc27d8d55daa97c6813dc86381d
Sandeep <sandeep@ubuntu18.domain.name>
2020-07-18 13:38:32 +0530

    Removed edit this page on github link and removed text banner in code block.

---
+++ src/@rocketseat/gatsby-theme-docs/components/Docs/EditGithub.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+export default function EditGithub({ githubEditUrl }) {
+  return null;
+}
+
+EditGithub.propTypes = {
+  githubEditUrl: PropTypes.string,
+};
+
+EditGithub.defaultProps = {
+  githubEditUrl: null,
+};
